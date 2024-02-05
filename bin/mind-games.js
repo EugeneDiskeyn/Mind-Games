234c46759c8690167acd20b41e0469ceb10ea303
Eugene <jdorovko@gmail.com>
2024-02-05 23:30:48 +0300

    Step 4

--- bin/mind-games.js
+++ bin/mind-games.js
@@ -1,7 +1,18 @@
 const cli = require("../src/cli");
+const evenNumGame = require("../src/evenNumGame");
+let isGameContinue = true;
 
 console.log("node bin/brain-games.js");
 console.log("Welcome to the Brain Games!");
 
 const name = cli.getName();
 console.log("Hello,", name);
+
+while (isGameContinue) {
+    isGameContinue = evenNumGame.evenNumGame();
+    break;
+}
+
+if (isGameContinue !== true) {
+    console.log("Let's try again, " + name + " !");
+}
--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,4 @@
-exports.getName = function() {
+exports.getName = () => {
     const readlineSync = require("../lib/readline-sync");
     return readlineSync.question("Type your name here: ");
 }
